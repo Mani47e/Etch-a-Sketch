3af4258b4672a5cbe97ca1d48334f839fc18a272
Mani <maniabburi486@gmail.com>
2026-01-07 17:34:48 +0530

    Basic Stucture Done and Working

--- script.js
+++ script.js
@@ -1,82 +1,76 @@
-const box=[];
+const box = [];
 const container = document.querySelector("#container");
 let isMouseDown_box = false;
 const reset_button = document.querySelector(".reset_button")
 let size = 4;
 const containerheight = 540;
-
-//creating box
-for (let i = 0; i < size*size; i++) {
-    box[i] = document.createElement("divs");
-   
-    box[i].style.width = containerheight/4+"px";
-    box[i].style.height = containerheight/4+"px";
-    box[i].style.margin="0px";
-    box[i].style.backgroundColor = "red";
-   
-    box[i].classList.add("box");  
-   
-    container.appendChild(box[i]);  
-}
-
-
-
-
-//Range slider
 const square_num = document.querySelector("#box_size");
 const Gridsize = document.querySelector("#Grid_size");
-Gridsize.textContent="4x4";
+Gridsize.textContent = "4x4";
 
-square_num.addEventListener("input",()=>{
+createGrid(4);
 
-    Gridsize.textContent=square_num.value+"x"+square_num.value;
-     size = Number(square_num.value);
+//Range slider
+square_num.addEventListener("input", () => {
 
+    Gridsize.textContent = square_num.value + "x" + square_num.value;
+    size = Number(square_num.value);
+
+    createGrid(size);
 })
 
 
 
 
 //Hover Effects
-for (let i = 0; i < size*size; i++) {
-box[i].addEventListener("mousedown",()=>{
-     isMouseDown_box = true;
-         box[i].style.backgroundColor = "black";
-
+document.addEventListener("mouseup", () => {
+    isMouseDown_box = false;
 })
-}
 
-
-for (let i = 0; i < size*size; i++) {
-        box[i].addEventListener("mouseover",()=>{
-           
-                  if(isMouseDown_box){
-                      box[i].style.backgroundColor = "black";
-              }
-                })
-}
-
-
-document.addEventListener("mouseup",()=>{
-        isMouseDown_box=false;
-})
- 
 
 //Reset Color
+reset_button.addEventListener("click", () => {
 
-reset_button.addEventListener("click",()=>{
-
-    for (let i = 0; i < size*size; i++) {
-            box[i].style.backgroundColor = "blue";
+    for (let i = 0; i < box.length; i++) {
+        box[i].style.backgroundColor = "blue";
     }
 
 })
 
 
+//Create Function to make grid
+
+function createGrid(size) {
+    container.innerHTML = "";
+    box.length = 0;
 
 
+    for (let i = 0; i < size * size; i++) {
+        box[i] = document.createElement("div");
+
+        box[i].style.width = containerheight / size + "px";
+        box[i].style.height = containerheight / size + "px";
+        box[i].style.margin = "0px";
+        box[i].style.backgroundColor = "red";
+
+        box[i].classList.add("box");
+
+        container.appendChild(box[i]);
+
+        //mousedown
+        box[i].addEventListener("mousedown", () => {
+            isMouseDown_box = true;
+            box[i].style.backgroundColor = "black";
+        })
+
+        //Mousehover
+        box[i].addEventListener("mouseover", () => {
+
+            if (isMouseDown_box) {
+                box[i].style.backgroundColor = "black";
+            }
+        })
+    }
 
 
-
-
-
+}
